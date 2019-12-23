415c4850542de724c55a22af02a426f145a2fb08
Iuliia Tsentylo <labliuk.maksym@gmail.com>
2019-12-23 18:19:15 +0200

    Updates

---
+++ precache-manifest.d3ac4d75f775d3b62757f7880e9ce716.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba9b077ba4531774afce3d68d589be77",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "54782f9571c001ab09f7",
+    "url": "/react_dynamic-list-of-posts/static/css/main.d63e8f8b.chunk.css"
+  },
+  {
+    "revision": "a93dcb5f4871ee723658",
+    "url": "/react_dynamic-list-of-posts/static/js/2.a2bac020.chunk.js"
+  },
+  {
+    "revision": "54782f9571c001ab09f7",
+    "url": "/react_dynamic-list-of-posts/static/js/main.c61043bf.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
